68af6941ed780b7165f9bceaa4286573f0c1fa40
Akash7392 <akashyesankar999@gmail.com>
2020-09-02 11:18:06 +0530

    edit a class

--- Jenkin/src/main/java/jenkin_test/Jenkin_first_test.java
+++ Jenkin/src/main/java/jenkin_test/Jenkin_first_test.java
@@ -1,17 +1,21 @@
 package jenkin_test;
 
+
+
 public class Jenkin_first_test {
 
 	public static void main(String[] args) {
 
-		int i;
+		int i,b;
 		
 		for(i=0;i <10;i++)
 		{
 			System.out.println(+i);
-			
+				
 		}
 
+		Jenkin_first_test d = new Jenkin_first_test();
+	
 	}
 
 }
